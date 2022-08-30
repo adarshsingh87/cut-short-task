e716f686a75a5878f4749bac73b1354869be2c08
Adarsh singh <adarshmanoj.05.as@gmail.com>
2022-08-30 21:06:16 +0530

    completed: onboarding completed

--- src/pages/onboarding/onboarding.jsx
+++ src/pages/onboarding/onboarding.jsx
@@ -7,6 +7,7 @@ import NameInputs from '../../components/name-inputs-ob1/name-inputs'
 
 // assets import
 import Logo from '../../assets/logo.png'
+import CongratsCheck from '../../assets/congrats-check.png'
 
 // css imports
 import './onboarding.css'
@@ -14,13 +15,16 @@ import WorkspaceInputs from '../../components/workspace-inputs/workspace-inputs'
 import UseOb3 from '../../components/use-ob3/use-ob3'
 
 const Onboarding = () => {
-  const [progress, setProgress] = useState(3)
+  const [progress, setProgress] = useState(1) // progress state
+  // states for 1st step
   const [name, setName] = useState('')
   const [dpName, setDpName] = useState('')
+  // states for 2nd step
   const [workspaceName, setWorkspaceName] = useState('')
   const [workspaceURL, setWorkspaceUrl] = useState('')
-  const [use, setUse] = useState('self')
+  const [use, setUse] = useState('self') // state for 3rd step
 
+  // headings & sub headings for onboarding
   const onboardingHeader = [
     { heading: 'Welcome! First things first...', subHeading: 'You can always change them later.' },
     {
@@ -40,7 +44,7 @@ const Onboarding = () => {
   const handleOnboardingClick = () => {
     if (progress === 1 && (name === '' || dpName === '')) {
       window.alert('Please Fill the required fields')
-    } else if (progress === 2 && workspaceName ==='') {
+    } else if (progress === 2 && workspaceName === '') {
       window.alert('Please Fill the required fields')
     } else if (progress < 4) {
       setProgress(progress + 1)
@@ -54,8 +58,11 @@ const Onboarding = () => {
     <div className='onboarding__container'>
       <img src={Logo} alt='Eden Logo' className='onboarding__logo' />
       <ProgressTracker step={progress} />
+      {progress === 4 ? (
+        <img src={CongratsCheck} alt='Eden Congrats check' className='onboarding__congrats' />
+      ) : null}
       <h1 className='onboarding__heading'>
-        {onboardingHeader[progress - 1].heading} {progress === 4 ? `${name}!` : null}
+        {onboardingHeader[progress - 1].heading} {progress === 4 ? `${dpName}!` : null}
       </h1>
       <p className='onboarding__sub-heading'>{onboardingHeader[progress - 1].subHeading}</p>
       {progress === 1 ? (
@@ -69,14 +76,9 @@ const Onboarding = () => {
           setWorkspaceUrl={setWorkspaceUrl}
         />
       ) : null}
-      {progress === 3 ? (
-        <UseOb3
-          use={use}
-          setUse={setUse}
-        />
-      ) : null}
+      {progress === 3 ? <UseOb3 use={use} setUse={setUse} /> : null}
       <button onClick={handleOnboardingClick} className='onboarding__c2a-btn'>
-        Create Workspace
+        {progress === 4 ? 'Launch Eden' : 'Create Workspace'}
       </button>
     </div>
   )
